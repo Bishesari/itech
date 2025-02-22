16c9c0fef18b4f2088c053c030515adcd0c71f2e
Yasser <Yasser.Bishesari@gmail.com>
2025-02-22 14:22:57 +0330

    I-Tech Assist

--- resources/views/components/my/bottom-fixed-footer.blade.php
+++ resources/views/components/my/bottom-fixed-footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="flex justify-center items-center border-t h-[50px] fixed bottom-0 w-full" style="font-family: Noora_Latin,serif">
     <p class="text-xs font-semibold text-center text-gray-700">
-        I-Tech v{{__('11.4.5')}} - ( Laravel v{{ Illuminate\Foundation\Application::VERSION }} ) - ( PHP v{{ PHP_VERSION }} )
+        I-Tech v{{config('app.ver')}} - ( Laravel v{{ Illuminate\Foundation\Application::VERSION }} ) - ( PHP v{{ PHP_VERSION }} )
         <br>
         Contact: +98 903 433 6111 &nbsp;&nbsp;&nbsp;&nbsp; Yasser.Bishesari@Gmail.com
     </p>
